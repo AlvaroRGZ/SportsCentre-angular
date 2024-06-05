63b81a30592568d8a7a4b71cedf862b33cad381e
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-05 20:43:02 +0100

    Added environment variables for firebase auth

---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1iV7nT8UZVM50-0eIro-OVIp9XglJfcs",
+  authDomain: "sportscentre-e4ae1.firebaseapp.com",
+  projectId: "sportscentre-e4ae1",
+  storageBucket: "sportscentre-e4ae1.appspot.com",
+  messagingSenderId: "751757951556",
+  appId: "1:751757951556:web:7c8dfb91aeab822bb9f211",
+  measurementId: "G-L0MJ6Z6WF0"
+};
